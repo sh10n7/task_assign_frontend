71eb8238f796d65b6f71f6b86e655f513f7a19a7
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-01 13:52:30 +0900

    ログアウトボタンの実装

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,11 +1,41 @@
 <template>
   <h1>タスク管理アプリ</h1>
+  <p>こんにちは{{ displayName }}さん</p>
+  <button @click="handleSignOut">ログアウト</button>
+  <form>
+    <input type="text" id="content" placeholder="content" v-model="content">
+    <select>
+      
+    </select>
+    <button value="新規登録" @click.prevent="handleSignUp">新規登録</button>
+  </form>
+  <TaskList />
 </template>
 
 <script>
+import TaskList from './TaskList.vue'
+import { auth, signOut } from '../firebase';
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default {
-  name: 'Top',
+  name: 'TopComp',
+  components: {
+    TaskList
+  },
+  setup() {
+    const currentUser = auth.currentUser;
+    const displayName = computed(() => currentUser.displayName);
+    const router = useRouter();
+    
+
+    const handleSignOut = async () => {
+      await signOut(auth);
+      router.push("/");
+    };
+
+    return { displayName, handleSignOut }
+  }
 }
 </script>
 
